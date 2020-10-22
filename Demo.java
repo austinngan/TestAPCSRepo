f5411da10ee293b1a7feeb1739e0c17e0459f351
austinngan <angan20@stuy.edu>
2020-10-21 20:56:22 -0400

    I added the new methods required in assignment 10

--- Demo.java
+++ Demo.java
@@ -1,12 +1,13 @@
 public class Demo{
   public static void main(String[]args){
     int num = 0;
+    int[][] a1={ {1,2,3}, {4,5,6}, {2} };
     if (args.length==0){
       num=5;
     }
     else{
-    num = Integer.parseInt(args[0]);
-  }
+      num = Integer.parseInt(args[0]);
+    }
       Demo.printLoop(num);
     }
   public static void printLoop(int x){
@@ -16,5 +17,46 @@ public class Demo{
         }
         System.out.println();
       }
+    }
+    public static String arrToString(int[] arr){
+      String str="{";
+      if (arr.length==0){
+        return "{}";
+      }
+      for (int i=0;i<arr.length;i++){
+        str=str+arr[i]+", ";
+        if (i==arr.length-1){
+          str=str+"}";
+          str=str.substring(0,str.length()-3)+"}";
+        }
+      }
+      return str;
+    }
+    public static String arrayDeepToString(int[][] arr){
+      String newstring="";
+      for (int r=0;r<arr.length;r++){
+        newstring=newstring+Demo.arrToString(arr[r])+" \n";
+      }
+      return newstring;
+    }
+    public static int[][] create2DArray(int rows, int cols, int maxvalue){
+      int[][] arr=new int[rows][cols];
+      for (int i=0;i<arr.length;i++){
+        for (int y=0;y<arr[i].length;y++){
+          arr[i][y]=((int)(Math.random()*(maxvalue+1)));
+        }
+      }
+      return arr;
+    }
+    public static int[][] create2DArrayRandomized(int rows, int cols, int maxvalue){
+      int[][] arr=new int[rows][];
+      for (int i=0;i<arr.length;i++){
+        arr[i]=new int[cols];
+        cols=(int)(Math.random()*cols);
+        for (int y=0;y<arr[i].length;y++){
+          arr[i][y]=((int)(Math.random()*maxvalue));
+        }
+      }
+      return arr;
     }
   }
